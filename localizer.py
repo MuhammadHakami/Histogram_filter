7ce26c224b46b9e971d5f576d1a3657719d7e921
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-04-12 09:56:53 +0300

    finished histogram filter

---
+++ localizer.py
@@ -0,0 +1,44 @@
+# import pdb
+from helpers import normalize, blur
+
+def initialize_beliefs(grid):
+    height = len(grid)
+    width = len(grid[0])
+    area = height * width
+    belief_per_cell = 1.0 / area
+    beliefs = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            row.append(belief_per_cell)
+        beliefs.append(row)
+    return beliefs
+
+def sense(color, grid, beliefs, p_hit, p_miss):
+    #
+    # TODO - implement this in part 2
+    # Pure python
+    new_beliefs = grid
+    for r in range(len(beliefs)):
+        for c in range(len(beliefs[0])):
+            hit=(color==grid[r][c])
+            new_beliefs[r][c]=beliefs[r][c]*(hit*p_hit+(1-hit)*p_miss)
+    s=0
+    for i in new_beliefs:
+        s+=sum(i)
+    for r in range(len(new_beliefs)):
+        for c in range(len(new_beliefs)):
+            new_beliefs[r][c]/=s
+    return (new_beliefs)
+
+def move(dy, dx, beliefs, blurring):
+    height = len(beliefs)
+    width = len(beliefs[0])
+    new_G = [[0.0 for i in range(width)] for j in range(height)]
+    for i, row in enumerate(beliefs):
+        for j, cell in enumerate(row):
+            new_i = (i + dy ) % height
+            new_j = (j + dx ) % width
+            # pdb.set_trace()
+            new_G[int(new_i)][int(new_j)] = cell
+    return blur(new_G, blurring)
